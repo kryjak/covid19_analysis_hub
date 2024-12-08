15d500a56ec0d2010d6fa167824718195864e676
Jakub Krys <jakkrys@gmail.com>
2024-12-08 17:32:06 +0100

    Update setup.sh to install R files

--- setup.sh
+++ setup.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-Rscript r_requirements.R
+
+# Make the script exit on any error
+set -e
+
+# Print commands as they are executed
+set -x
+
+# Check R installation
+R --version
+
+# Run R requirements and save output to a log file
+Rscript r_requirements.R 2>&1 | tee r_install.log
+
+# List installed R packages
+Rscript -e "installed.packages()" 2>&1 | tee installed_packages.log
+
+# Try to load epidatr specifically
+Rscript -e "library(epidatr)" 2>&1 | tee epidatr_test.log
